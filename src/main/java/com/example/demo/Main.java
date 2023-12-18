a6c8f1e18430d78d60e881c01bd83b5164809c5f
jelemiekun <jeremieeden1946@gmail.com>
2023-12-18 08:51:12 +0800

    added add button, duplicator checker, and emptyField prompt

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -7,12 +7,13 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Scene1.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Scene1.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.setTitle("Hello!");
         stage.show();
     }
--- src/main/java/com/example/demo/Scene1Controller.java
+++ src/main/java/com/example/demo/Scene1Controller.java
@@ -2,12 +2,21 @@ package com.example.demo;
 
 import com.example.Object.Person;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 
 public class Scene1Controller {
+
+    @FXML
+    private Button btnClearAll;
+
+    @FXML
+    private Button btnAdd;
 
     @FXML
     private ListView<String> listViewPerson;
@@ -16,17 +25,59 @@ public class Scene1Controller {
     private TextField txtFieldPerson;
 
     @FXML
-    void pressedEnter(KeyEvent event) {
+    private void btnAddClick(MouseEvent event) {
+        addAPerson();
+    }
+
+    @FXML
+    private void pressedEnter(KeyEvent event) {
         if (event.getCode() == KeyCode.ENTER) {
-            if (!txtFieldPerson.getText().isEmpty()) {
-                addAPerson(txtFieldPerson.getText());
-                txtFieldPerson.setText("");
+            addAPerson();
+        }
+    }
+
+    @FXML
+    private void btnClearAllClick(MouseEvent event) {
+        if (listViewPerson.getItems().size() != 0) {
+            for (int i = listViewPerson.getItems().size() - 1; i >= 0; i--) {
+                listViewPerson.getItems().remove(i);
             }
         }
     }
 
-    private void addAPerson(String name) {
-        Person person = new Person(name);
-        listViewPerson.getItems().add(person.getName());
+    private void addAPerson() {
+        if (!txtFieldPerson.getText().isEmpty()) {
+            String name = txtFieldPerson.getText();
+            if (listViewPerson.getItems().contains(name)) {
+                duplicateName();
+            } else {
+                Person person = new Person(name);
+                listViewPerson.getItems().add(person.getName());
+                clearField();
+            }
+        } else {
+            emptyField();
+        }
+    }
+
+    private void emptyField() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Error");
+        alert.setHeaderText("Empty Field");
+        alert.setContentText("Can not add. Field is empty.");
+        alert.show();
+    }
+
+    private void duplicateName() {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText("Duplicate Name");
+        alert.setContentText("You can't enter a duplicate name");
+        alert.show();
+        clearField();
+    }
+
+    private void clearField() {
+        txtFieldPerson.setText("");
     }
 }
